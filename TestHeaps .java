978d818fe400e19cc8663c3d7b885b1354bfbcad
Wilson Turpo Huanca <wturpoh@unsa.edu.pe>
2023-06-17 06:23:13 -0500

    Instaciamos las clases con datos genericos

---
+++ TestHeaps .java
@@ -0,0 +1,10 @@
+public class TestHeaps {
+
+    public static void main(String[] args) {
+        
+        Heap<Integer> monticulo = new Heap<>();
+
+        PriorityQueueHeap<Integer> heapPrioritario = new PriorityQueueHeap<>();
+       
+    }
+}
